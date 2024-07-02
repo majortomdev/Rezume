042c262528cd628fd9072b7ceb810711fef5dd38
Joseph Kinahan <kinahanj25@gmail.com>
2024-07-02 23:27:30 +0100

    first commit- /w spring boot be bare bones

---
+++ RezumeBE/src/main/java/com/majortomdev/RezumeBE/RezumeBeApplication.java
@@ -0,0 +1,13 @@
+package com.majortomdev.RezumeBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezumeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezumeBeApplication.class, args);
+	}
+
+}
---
+++ RezumeBE/src/test/java/com/majortomdev/RezumeBE/RezumeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortomdev.RezumeBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezumeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
