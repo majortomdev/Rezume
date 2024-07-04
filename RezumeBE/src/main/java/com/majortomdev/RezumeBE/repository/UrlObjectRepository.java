659034a34c3200a16cc3bf55d09fc01ef3c4bf76
Joseph Kinahan <kinahanj25@gmail.com>
2024-07-04 21:07:59 +0100

    added in react FE

--- RezumeBE/src/main/java/com/majortomdev/RezumeBE/repository/UrlObjectRepository.java
+++ RezumeBE/src/main/java/com/majortomdev/RezumeBE/repository/UrlObjectRepository.java
@@ -1,12 +1,7 @@
 package com.majortomdev.RezumeBE.repository;
 
 import com.majortomdev.RezumeBE.model.UrlObject;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 
